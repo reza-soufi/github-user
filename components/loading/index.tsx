17157507e13677124089b57079a212907fd4ab82
Reza Soufi <reza.soufi2014@gmail.com>
2023-07-20 17:24:42 +0330

    add loading component

---
+++ components/loading/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./loading.module.scss";
+
+const Loading = () => {
+    return (
+        <div className={styles.spinner_square}>
+            <div className={`${styles.square_1} ${styles.square}`}></div>
+            <div className={`${styles.square_2} ${styles.square}`}></div>
+            <div className={`${styles.square_3} ${styles.square}`}></div>
+        </div>
+    );
+};
+
+export default Loading;
